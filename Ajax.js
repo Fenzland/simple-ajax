7654833a2d666a4348b0881c75e19c43f48bcaf5
Fenz <uukooo@126.com>
2016-11-29 21:24:54 +0800

    Add Main file

---
+++ Ajax.js
@@ -0,0 +1,103 @@
+(( global, mod )=>(global.define && global.define.amd)?global.define(mod):global.Ajax=mod())(this,()=>{
+	"use strict";
+
+	const httpRequest= ( method, url, headers, body )=>Object.assign(
+		new Promise(
+			( resolve, reject )=>{
+				const xhr= new XMLHttpRequest;
+				xhr.onreadystatechange= ()=>{
+					if( xhr.readyState==4 ){
+						resolve({
+							httpStatus: xhr.status,
+							response: xhr.responseText,
+						});
+					}
+				}
+				xhr.open(method,url,true);
+
+				for( name in headers ){
+					xhr.setRequestHeader(name,headers[name]);
+				}
+
+				xhr.send(body);
+			}
+		)
+		,
+		{
+			when( status, callback ){
+				return this.then(( { httpStatus, response, } )=>(status===httpStatus?callback(response):null,{ httpStatus, response, })),this;
+			},
+		}
+	);
+
+	return {
+
+		OPTIONS( url, headers={} )
+		{
+			return httpRequest( 'HEAD', url, headers, null );
+		},
+
+		HEAD( url, headers={} )
+		{
+			return httpRequest( 'HEAD', url, headers, null );
+		},
+
+		GET( url, headers={} )
+		{
+			return httpRequest( 'GET', url, headers, null );
+		},
+
+		DELETE( url, headers={} )
+		{
+			return httpRequest( 'DELETE', url, headers, null );
+		},
+
+		POST( url, body, headers={} )
+		{
+			return httpRequest( 'POST', url, headers, body );
+		},
+
+		PUT( url, body, headers={} )
+		{
+			return httpRequest( 'PUT', url, headers, body );
+		},
+
+		PATCH( url, body, headers={} )
+		{
+			return httpRequest( 'PATCH', url, headers, body );
+		},
+
+		get( url, params={} )
+		{
+			return httpRequest( 'POST', buildQuery(params,url) );
+		},
+
+		post( url, params={} )
+		{
+			return httpRequest( 'POST', url, {"Content-Type":"application/x-www-form-urlencoded",}, buildQuery(params) );
+		},
+
+		put( url, params={} )
+		{
+			return httpRequest( 'PUT', url, {"Content-Type":"application/x-www-form-urlencoded",}, buildQuery(params) );
+		},
+
+		patch( url, params={} )
+		{
+			return httpRequest( 'PATCH', url, {"Content-Type":"application/x-www-form-urlencoded",}, buildQuery(params) );
+		},
+
+	};
+
+	function buildQuery( parameters, url='' )
+	{
+		let delimiter= url? (url.indexOf('?')+1? '&' : '?') : '';
+
+		for( let key in parameters )
+		{
+			url+= delimiter+key+'='+encodeURIComponent(parameters[key]);
+			delimiter='&';
+		}
+		return url;
+	}
+});
